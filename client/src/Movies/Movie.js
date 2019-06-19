5233d2abadefdf75b7d82af3ce645bbd534fc0d2
Sheila R Moore <sherexmykes@github.com>
2019-06-19 00:43:19 -0400

    working on stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import MovieCard from './MovieCard';
+import { Link } from 'react-router-dom';
 export default class Movie extends Component {
   constructor(props) {
     super(props);
@@ -26,16 +27,17 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+   componentWillReceiveProps(newProps){
+   if(this.props.match.params.id !== newProps.match.params.id){
+       this.fetchMovie(newProps.match.params.id);
+     }
+  }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+   saveMovie = () => {
+     const addToSavedList = this.props.addToSavedList;
+     addToSavedList(this.state.movie)
+     console.log(this.state);
+   }
 
   render() {
     if (!this.state.movie) {
@@ -44,25 +46,8 @@ export default class Movie extends Component {
 
     const { title, director, metascore, stars } = this.state.movie;
     return (
-      <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
-        <div className="save-button">Save</div>
-      </div>
+      <MovieCard movie={this.state.movie}/>
+      
     );
   }
 }
